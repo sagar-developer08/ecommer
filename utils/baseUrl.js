5f9af0031099c820d34f17338aa66fd74e9574c1
sagar-developer08 <138370173+sagar-developer08@users.noreply.github.com>
2023-07-17 02:57:51 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,3 @@
-const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://livani-react.envytheme.com' 
-: 'http://localhost:3000';
+const baseUrl = process.env.VERCEL_URL || 'http://localhost:3000';
 
 export default baseUrl;
